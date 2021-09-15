43072a354c1bc0de1bbc9e82a02542bd4e321f33
Liliia Saleck <lilisaleck@gmail.com>
2021-09-15 15:54:25 +0200

    Finish testing offersService. Passed a test

--- src/offers.service.spec.ts
+++ src/offers.service.spec.ts
@@ -1,11 +1,30 @@
 import { Test } from '@nestjs/testing';
 import { OffersService } from './offers/offers.service';
 import { OffersRepository } from './offers/offers.reporitory';
-import { OffersModule } from './offers/offers.module';
+import { NotFoundException } from '@nestjs/common';
 
 const mockOffersRepository = () => ({
   getAllOffers: jest.fn(),
+  findOne: jest.fn(),
+  createOffer: jest.fn(),
+
 });
+
+const mockOffer = {
+  id: 'someId',
+  title: 'Test',
+  salaryFrom: 1,
+  salaryTo: 2,
+  location: 'Test',
+  tech: 'Test',
+  logo: 'Test',
+  experience: 'Test',
+  companySize: 3,
+  companyName: 'Test',
+  employmentType: 'Test',
+  jobDescription: 'Test',
+  aboutCompany: 'Test',
+};
 
 describe('OffersService', () => {
   let offersService: OffersService;
@@ -24,12 +43,36 @@ describe('OffersService', () => {
   });
 
   describe('getAllOffers', () => {
-    it('calls OffersRrepository.getAllOffers and returns the result', () => {
-      expect(offersRepository.getAllOffers).not.toHaveBeenCalled();
+    it('calls OffersRrepository.getAllOffers and returns the result', async () => {
       offersRepository.getAllOffers.mockResolvedValue('someValue');
-      const result = offersService.getAllOffers(null);
-      expect(offersRepository.getAllOffers).toHaveBeenCalled();
-      expect(result).toEqual('...something');
+      const result = await offersService.getAllOffers(null);
+      expect(result).toEqual('someValue');
+    });
+  });
+
+
+  describe('getOfferById', () => {
+    it('calls TasksRepository.findOne and returns the result', async () => {
+
+      offersRepository.findOne.mockResolvedValue(mockOffer);
+      const result = await offersService.getOfferById('someId');
+      expect(result).toEqual(mockOffer);
+    });
+
+    it('calls TasksRepository.findOne and handles an error', async () => {
+      offersRepository.findOne.mockResolvedValue(null);
+      expect(offersService.getOfferById('someId')).rejects.toThrow(
+        NotFoundException,
+      );
+    });
+  });
+
+  describe('createOffer', () => {
+    it('calls OffersRrepository.createOffer and returns the result', async () => {
+      
+      offersRepository.createOffer.mockResolvedValue('createdOffer');
+      const result = await offersService.createOffer(mockOffer);
+      expect(result).toEqual('createdOffer');
     });
   });
 });
