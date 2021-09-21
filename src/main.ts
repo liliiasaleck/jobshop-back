ba3ec31e6e9064b84f4b2e3f4caa0b90a3a4e3ce
Liliia Saleck <lilisaleck@gmail.com>
2021-09-21 15:54:13 +0200

    Change port

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-
 async function bootstrap() {
-  const logger = new Logger();
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: true,
+      preflightContinue: false,
+    },
+  });
   app.useGlobalPipes(new ValidationPipe());
-  const port = process.env.PORT;
-  await app.listen(port || 3000);
-  logger.log(`Application listening on port ${port}`);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
