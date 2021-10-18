292a3834de8d37414b3d36057141dfe4e3da4012
Liliia Saleck <lilisaleck@gmail.com>
2021-10-18 10:56:33 +0300

    Add logo entity

--- src/app/offers/offers.module.ts
+++ src/app/offers/offers.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/app/auth/auth.module';
+import Logo from './logo.entity';
 import { OffersController } from './offers.controller';
 import { OffersRepository } from './offers.reporitory';
 import { OffersService } from './offers.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([OffersRepository]), AuthModule],
+    TypeOrmModule.forFeature([OffersRepository, Logo]), AuthModule],
   controllers: [OffersController],
   providers: [OffersService],
 })
